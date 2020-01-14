85e62fb27aec3e0ebc6c542059132eac6694ce5e
Seb Bacon <seb.bacon@gmail.com>
2020-01-14 08:59:30 +0000

    Initial commit

---
+++ notebooks/analysis.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+
+# poopy doo
+
+
+# +
+from libs import foo
+
+
